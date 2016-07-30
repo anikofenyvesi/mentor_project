db2f5a7e21d6ab21870b3aa7aa17a1e2e9526466
Aniko Fenyvesi <fenyvesi@hu-bu-dhcp13892.emea.nsn-net.net>
2016-07-30 20:48:08 +0200

    creating models

--- mentor_candidates/models.py
+++ mentor_candidates/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Mentor(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=50)
+    description = models.TextField(blank=True)
+    
+class Opinion(models.Model):
+    question = models.ForeignKey(Mentor)
+    name = models.CharField(max_length=50)
+    description = models.TextField()
